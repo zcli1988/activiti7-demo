092350067b5778e2eb0668f7e29e71a980b6089e
gitwk <13282129660@163.com>
2019-08-27 08:40:36 +0800

    add task

---
+++ src/main/java/org/activiti/examples/controller/TaskController.java
@@ -0,0 +1,32 @@
+package org.activiti.examples.controller;
+
+import org.activiti.examples.service.TaskService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wangkai
+ * @since JDK8
+ */
+@RestController
+@RequestMapping("/task/")
+public class TaskController {
+
+    @Resource
+    private TaskService taskService;
+
+    @RequestMapping(value = "task", method = RequestMethod.GET)
+    public Object getTaskList() {
+        return taskService.getTaskList();
+    }
+
+    @RequestMapping(value = "task/claim", method = RequestMethod.GET)
+    public Object claim(@RequestParam String taskId) {
+        return taskService.claim(taskId);
+    }
+
+}
